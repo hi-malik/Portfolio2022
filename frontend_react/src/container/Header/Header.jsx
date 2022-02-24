7da3664b5bf7a8646a7e23b45b10ead711f3290e
Himanshu Malik <jsm.28415@gmail.com>
2022-02-24 19:41:07 +0530

    header-section complete

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className = "app__header app__flex">
+    <div id="home" className = "app__header app__flex">
       <motion.div
         whileInView={{ x : [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
