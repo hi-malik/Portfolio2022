cfc4948b7048b8aebc1bc658d86bd46c16c4a2b2
Himanshu Malik <jsm.28415@gmail.com>
2022-02-25 08:38:15 +0530

    about-section started

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import './About.scss';
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text"> 
+        I know that
+        <span>Problem solving</span>
+        equivalents to
+        <span>improving Algorithm</span>
+      </h2>
+    </>
   )
 }
 
