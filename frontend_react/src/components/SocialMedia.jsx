a91026cf96999542bb452673b24792cc93bfb23e
Himanshu Malik <jsm.28415@gmail.com>
2022-02-25 14:56:18 +0530

    All-Done

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
-import { BsLinkedin } from 'react-icons/bs';
+import { BsLinkedin, BsYoutube } from 'react-icons/bs';
 
 const SocialMedia = () => {
   return (
     <div className = "app__social">
         <div>
-            <BsTwitter />
+            <a href="https://twitter.com/MalikHacking" target="_blank" className="p-text"><BsTwitter /></a>
+        </div>
+        <div> 
+            <a href="https://www.linkedin.com/in/himanshu-malik-54728917a/" target="_blank" className="p-text"><BsLinkedin /></a>
         </div>
         <div>
-            <BsLinkedin />
+            <a href="https://www.instagram.com/its_himanshu2000/" target="_blank" className="p-text"><BsInstagram /></a>
         </div>
         <div>
-            <BsInstagram />
+            <a href="https://www.youtube.com/channel/UCB56-AiILovestYUPiGq8ng" target="_blank" className="p-text"><BsYoutube /></a>
         </div>
         
     </div>
