91fdd95d1ce6fc79635b70a9bc25d903b676767f
Himanshu Malik <jsm.28415@gmail.com>
2022-02-25 09:40:47 +0530

    about-sections done

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
+
 import './About.scss';
+
+const abouts = [
+  { title: 'Data Structure & Algorithms', description: 'I love to solve Algorithms', imgUrl: images.about01 },
+  { title: 'Web Developer', description: 'I am a Web Developer', imgUrl: images.about02 },
+  { title: 'Block-Chain Developer', description: 'Bringing the power in our hands', imgUrl: images.about03 },
+  { title: 'Machine Learning', description: 'Teaching machines how to boil water', imgUrl: images.about04 },
+
+]
 
 const About = () => {
   return (
     <>
-      <h2 className="head-text"> 
-        I know that
-        <span>Problem solving</span>
-        equivalents to
-        <span>improving Algorithm</span>
-      </h2>
+      <h2 className="head-text"> I know that <span>Problem solving</span><br />equivalents to<br /><span>Practicing Algorithm</span></h2>
+
+      <div className = "app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{opacity: 1}}
+            whileHover={{scale: 1.1}}
+            transition={{duration: 0.5, type: 'tween'}}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title}/>
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
     </>
   )
 }
