4fa4cd8328ee528d2e53cc0043eb8a841e152c7b
Himanshu Malik <jsm.28415@gmail.com>
2022-02-25 13:59:38 +0530

    Skill Section completed

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 
 import './About.scss';
 import { urlFor, client } from '../../client';
@@ -41,4 +41,4 @@ const About = () => {
   )
 }
 
-export default AppWrap(About, 'about');
+export default AppWrap(MotionWrap(About, 'app__about'), 'about', "app__whitebg");
--- frontend_react/src/wrapper/index.js
+++ frontend_react/src/wrapper/index.js
@@ -1,1 +1,7 @@
-export { default as AppWrap } from './AppWrap';
+import AppWrap from './AppWrap';
+import MotionWrap from './MotionWrap';
+
+export {
+  AppWrap,
+  MotionWrap,
+};
